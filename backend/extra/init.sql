1a348e82d323624601e72562a3a9e53bdc51dbf4
vkarvetsky <vkarvetsky@whoosh.bike>
2023-03-24 18:40:03 +0100

    add init sql migration for database

--- backend/extra/init.sql
+++ backend/extra/init.sql
@@ -1,18 +1,86 @@
--- CREATE DATABASE db_evraz;
+-- CREATE DATABASE db_marking;
 
-GRANT ALL PRIVILEGES ON DATABASE db_misis TO db_misis;
+grant all privileges on database db_misis to db_misis;
 
 \c db_misis;
 
-CREATE SCHEMA IF NOT EXISTS public;
+create schema if not exists marking;
 
-CREATE TABLE public.event(
-    id           bigserial
-        constraint event_pk
+create table marking.participant_reference
+(
+    inn         text
+        constraint inn_pk
             primary key,
-    created_at   timestamp with time zone not null,
-    exhauster_id int                      not null,
-    status       jsonb                    not null
+    region_code text not null
 );
 
-CREATE UNIQUE INDEX on public.event (exhauster_id, created_at);
+create table marking.product_reference
+(
+    gtin               text
+        constraint gtin_pk
+            primary key,
+    inn                text references marking.participant_reference (inn),
+    product_name       text not null,
+    product_short_name text not null,
+    tnved              text not null,
+    tnved10            text not null,
+    brand              text not null,
+    country            text,
+    volume             int
+);
+
+create index product_reference_inn_idx
+    on marking.product_reference (inn);
+
+create table marking.salepoint_reference
+(
+    id_sp          text
+        constraint id_sp_pk
+            primary key,
+    inn            text references marking.participant_reference (inn),
+    region_code    text not null,
+    city_with_type text,
+    city_fias_id   text,
+    postal_code    text
+);
+
+create index salepoint_reference_inn_idx
+    on marking.salepoint_reference (inn);
+
+create table marking.product_input
+(
+    dt             timestamp with time zone not null,
+    inn            text references marking.participant_reference (inn),
+    gtin           text references marking.product_reference (gtin),
+    prid           text references marking.participant_reference (inn),
+    operation_type text                     not null,
+    cnt            int                      not null
+);
+
+create index product_input_inn_idx
+    on marking.product_input (inn);
+
+create table marking.product_output
+(
+    dt             timestamp with time zone not null,
+    gtin           text references marking.product_reference (gtin),
+    prid           text references marking.participant_reference (inn),
+    inn            text references marking.participant_reference (inn),
+    id_sp          text references marking.salepoint_reference (id_sp),
+    type_operation text                     not null,
+    price          int                      not null,
+    cnt            int                      not null
+);
+
+create index product_output_inn_idx
+    on marking.product_output (inn);
+
+create table marking.product_movement
+(
+    dt           timestamp with time zone not null,
+    gtin         text references marking.product_reference (gtin),
+    prid         text references marking.participant_reference (inn),
+    sender_inn   text references marking.participant_reference (inn),
+    receiver_inn text references marking.participant_reference (inn),
+    cnt_moved    int                      not null
+);
