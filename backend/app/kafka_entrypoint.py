6498f1a343522db5518e06a7859413f1796c95cc
pibragimov <pibragimov@whoosh.bike>
2023-03-24 18:45:35 +0300

    ref: remove unused code

--- backend/app/kafka_entrypoint.py
+++
@@ -1,9 +0,0 @@
-from presentation.dependencies import container
-from presentation.kafka.main import listen_kafka
-
-if __name__ == "__main__":
-    exhausters_stubs = container.exhauster_service.exhausters_stubs
-    for exhauster in exhausters_stubs:
-        container.exhauster_service.mongo_repository.create_exhauster(exhauster=exhauster)
-
-    listen_kafka()
--- backend/app/kafka_entrypoint_store_localy.py
+++
@@ -1,4 +0,0 @@
-from presentation.kafka.main import listen_kafka_store_localy
-
-if __name__ == "__main__":
-    listen_kafka_store_localy()
