803a579988bc15f685d7377591617e941530175b
pibragimov <pibragimov@whoosh.bike>
2023-03-24 21:38:22 +0300

    feat: better script

--- backend/extra/prepare_dataset.py
+++ backend/extra/prepare_dataset.py
@@ -1,26 +1,29 @@
 import pandas as pd
+from pathlib import Path
 
-dataframe = pd.read_csv("product_reference.csv")
+base_path = Path("../app/data/datasets")
+
+dataframe = pd.read_csv(base_path / "product_reference.csv")
 dataframe.replace(to_replace="", value=None, inplace=True)
 dataframe.replace(to_replace="НЕ КЛАССИФИЦИРОВАНО", value=None, inplace=True)
-dataframe.to_csv("product_reference.csv", index=False)
+dataframe.to_csv(base_path / "product_reference.csv", index=False)
 
-dataframe = pd.read_csv("participant_reference.csv")
+dataframe = pd.read_csv(base_path / "participant_reference.csv")
 dataframe.replace(to_replace="", value=None, inplace=True)
-dataframe.to_csv("participant_reference.csv", index=False)
+dataframe.to_csv(base_path / "participant_reference.csv", index=False)
 
-dataframe = pd.read_csv("salepoint_reference.csv")
+dataframe = pd.read_csv(base_path / "salepoint_reference.csv")
 dataframe.replace(to_replace="", value=None, inplace=True)
-dataframe.to_csv("salepoint_reference.csv", index=False)
+dataframe.to_csv(base_path / "salepoint_reference.csv", index=False)
 
-dataframe = pd.read_csv("product_input.csv")
+dataframe = pd.read_csv(base_path / "product_input.csv")
 dataframe.replace(to_replace="", value=None, inplace=True)
-dataframe.to_csv("product_input.csv", index=False)
+dataframe.to_csv(base_path / "product_input.csv", index=False)
 
-dataframe = pd.read_csv("product_output.csv")
+dataframe = pd.read_csv(base_path / "product_output.csv")
 dataframe.replace(to_replace="", value=None, inplace=True)
-dataframe.to_csv("product_output.csv", index=False)
+dataframe.to_csv(base_path / "product_output.csv", index=False)
 
-dataframe = pd.read_csv("product_movement.csv")
+dataframe = pd.read_csv(base_path / "product_movement.csv")
 dataframe.replace(to_replace="", value=None, inplace=True)
-dataframe.to_csv("product_movement.csv", index=False)
+dataframe.to_csv(base_path / "product_movement.csv", index=False)
